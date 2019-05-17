3e4389e6a796a9415b5e501be789ecb9ba6b7bee
chichien97 <41542987+chichien97@users.noreply.github.com>
2019-05-17 13:24:29 +0800

    Create login.php

---
+++ php/login.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include "connection.php";
+$username = $_POST["username"];
+$password = $_POST["password"];
+$sql = 'SELECT * FROM login WHERE userName = "'.$username.'" AND passWord = "'.$password.'"';
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+  while ($row = $result->fetch_assoc()) {
+    if($row["status"] == "admin"){
+      $_SESSION["name"] = $row["name"];
+      $_SESSION["status"] = $row["status"];
+      // echo "admin";
+      header("location:../admin/home.php");
+    }
+    else if($row["status"] == "client"){
+      $_SESSION["name"] = $row["name"];
+      $_SESSION["status"] = $row["status"];
+      $_SESSION["id"] = $row["client_ID"];
+      // echo "client";
+      header("location:../client/home.php");
+    }
+    else{
+      // echo "error";
+      header("location:".$_SERVER["HTTP_REFERER"]);
+    }
+  }
+}
+mysqli_close($conn);
+?>
