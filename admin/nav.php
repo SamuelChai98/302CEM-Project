805f1ab72fc83f9605629825bc3c95e097c180ab
SamuelChai98 <41310652+SamuelChai98@users.noreply.github.com>
2019-06-11 13:28:31 +0800

    Update nav.php

--- admin/nav.php
+++ admin/nav.php
@@ -47,7 +47,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link">
+          <a href="../php/logout.php" class="nav-link">
             <i class="nav-icon fa fa-th"></i>
             <p>
               Logout
